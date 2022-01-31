985b425eb630881e16c5bf353253148fddd2ebc5
fspeter <fspeter@gmail.com>
2022-01-31 08:45:41 +0100

    Initialisation du projet

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container mx-auto bg-gray-300 p-8">
+    <h1 class="font-bold text-gray-600 text-lg">
+      Hello, Tailwind 3 on NuxtJS 3!
+    </h1>
+  </div>
+</template>
